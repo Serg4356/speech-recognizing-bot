104596c5826b1571dcd34a770f66ca7001dda873
Serg4356 <asdfg111000@yandex.ru>
2019-05-23 17:42:02 +0500

    Add DialofFlow api requests

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import os
 from dotenv import load_dotenv
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
+import requests
+import json
+from pprint import pprint
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -14,32 +17,92 @@ logger = logging.getLogger(__name__)
 # Define a few command handlers. These usually take the two arguments bot and
 # update. Error handlers also receive the raised TelegramError object in error.
 def start(bot, update):
-    """Send a message when the command /start is issued."""
+    '''Send a message when the command /start is issued.'''
     update.message.reply_text('Hi!')
 
 
 def help(bot, update):
-    """Send a message when the command /help is issued."""
+    '''Send a message when the command /help is issued.'''
     update.message.reply_text('Help!')
 
 
+def speach(bot, update):
+    '''Send a message as DialogFlow reply'''
+    url = 'https://api.dialogflow.com/v1/query'
+    token = os.getenv('dialog_flow_token')
+    headers = {
+        'Authorization': f'Bearer {token}',
+    }
+    params = {
+        'v': 20150910,
+        'sessionId': update.message.chat_id,
+        'query': update.message.text,
+        'lang': 'ru',
+    }
+    response = requests.get(url, headers=headers, params=params)
+    dialog_flow_response = response.json()
+    update.message.reply_text(dialog_flow_response['result']['fulfillment']['speech'])
+
+
+def deploy_intent(intent_name, intent):
+    '''Deploy new intents for DialogFlow Agent'''
+    url = 'https://api.dialogflow.com/v1/intents'
+    token = os.getenv('dialog_flow_developer_token')
+    headers = {
+        'Authorization': f'Bearer {token}',
+    }
+    payload = {
+        'v': 20150910,
+        'auto': True,
+        'contexts': [],
+        'name': intent_name,
+        'userSays':[],
+        'responses':[
+            {
+                'messages':[
+                    {
+                        'speech': intent['answer'],
+                    }
+                ],
+            },
+        ],
+    }
+    for question in intent['questions']:
+        user_say = {
+            'data': [
+                {
+                    'text': question,
+                }
+            ]
+        }
+        payload['userSays'].append(user_say)
+    response = requests.post(url, json=payload, headers=headers)
+    pprint(response.json())
+
+
+def deploy_intents_from_file(filename):
+    with open(filename, 'r') as question_file:
+        intents = json.load(question_file)
+    for intent_name, intent in intents.items():
+        deploy_intent(intent_name, intent)
+
+
 def echo(bot, update):
-    """Echo the user message."""
+    '''Echo the user message.'''
     update.message.reply_text(update.message.text)
 
 
 def error(bot, update, error):
-    """Log Errors caused by Updates."""
+    '''Log Errors caused by Updates.'''
     logger.warning('Update "%s" caused error "%s"', update, error)
 
 
 def main():
-    """Start the bot."""
+    '''Start the bot.'''
     # Create the EventHandler and pass it your bot's token.
-    load_dotenv()
     bot_token = os.getenv('bot_token')
     proxy = os.getenv('http_proxy')
-    updater = Updater(bot_token, rquest_kwargs={'proxy_url': proxy})
+    updater = Updater(bot_token, request_kwargs={'proxy_url': proxy})
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
@@ -49,7 +112,7 @@ def main():
     dp.add_handler(CommandHandler("help", help))
 
     # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
+    dp.add_handler(MessageHandler(Filters.text, speach))
 
     # log all errors
     dp.add_error_handler(error)
@@ -64,4 +127,7 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    #main()
+    load_dotenv()
+    #deploy_intents_from_file('questions.json')
+
